77aa65728ede46d9da5b87cc9b2380f8bd6f39f6
Jorge Pérez Bárcenas <jorge.barcenas@paramount.com>
2025-05-26 18:20:12 -0400

    [SETUP] Added the connection strings into the AppSettings [SETUP] Configured the DbContext Services in the App Program [SETUP] Configured the DataSeeder in the App Program

--- HrmApp.Web/Program.cs
+++ HrmApp.Web/Program.cs
@@ -1,9 +1,34 @@
+using HrmApp.Domain;
+using HrmApp.Domain.Seeders;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+builder.Services.AddDbContext<HrmAppDbContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("LocalDb")));
+
 builder.Services.AddRazorPages();
 
 var app = builder.Build();
+
+// Seed the database
+using (var scope = app.Services.CreateScope())
+{
+    var services = scope.ServiceProvider;
+    try
+    {
+        var context = services.GetRequiredService<HrmAppDbContext>();
+        context.Database.Migrate();
+
+        await DataSeeder.SeedEmployeesAsync(context, 25);
+    }
+    catch (Exception ex)
+    {
+        var logger = services.GetRequiredService<ILogger<Program>>();
+        logger.LogError(ex, "An error occurred while seeding the database.");
+    }
+}
 
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
